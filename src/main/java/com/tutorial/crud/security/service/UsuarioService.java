dfd38ab4dbbe6bda81b6bee98c4f06ef21e1654a
jack012es <jacksonmixproject@gmail.com>
2022-03-03 15:45:25 -0500

    End

---
+++ src/main/java/com/tutorial/crud/security/service/UsuarioService.java
@@ -0,0 +1,29 @@
+package com.tutorial.crud.security.service;
+
+import com.tutorial.crud.security.entity.Usuario;
+import com.tutorial.crud.security.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class UsuarioService {
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
+    public Optional<Usuario> getByNombreUsuario(String nombreUsuario){
+        return usuarioRepository.findByNombreUsuario(nombreUsuario);
+    }
+    public boolean existsByNombreUsuario(String nombreUsuario){
+        return  usuarioRepository.existsByNombreUsuario(nombreUsuario);
+    }
+    public boolean existsByEmail(String email){
+        return usuarioRepository.existsByEmail(email);
+    }
+    public void save(Usuario usuario){
+        usuarioRepository.save(usuario);
+    }
+}
---
+++ src/main/java/com/tutorial/crud/util/CreateRoles.java
@@ -0,0 +1,23 @@
+//package com.tutorial.crud.util;
+
+//import com.tutorial.crud.security.entity.Rol;
+//import com.tutorial.crud.security.enums.RolNombre;
+//import com.tutorial.crud.security.service.RolService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.stereotype.Component;
+
+//@Component
+//public class CreateRoles implements CommandLineRunner {
+
+  //  @Autowired
+    //RolService rolService;
+
+//    @Override
+  //  public void run(String... args) throws Exception {
+   //     Rol rolAdmin = new Rol(RolNombre.ROLE_ADMIN);
+     //   Rol rolUser = new Rol(RolNombre.ROLE_USER);
+       // rolService.save(rolAdmin);
+        //rolService.save(rolUser);
+    //}
+//}
